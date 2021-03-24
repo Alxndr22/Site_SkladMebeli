e30f26cfc305fb3eb8f8143c67a9cfcdb0967dd7
Alxndr22 <81303585+Alxndr22@users.noreply.github.com>
2021-03-24 22:11:08 +0200

    Edited

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*$(function () {
-    $(".menu_mobile").click(function() {
-        $(".dropdown_menu_container").toggleClass("active");
-        $("body").toggleClass("lock");
-        /!*return false;*!/
-    });
-});*/
-
 $(document).ready(function () {
     $("#menu_toggle").click(function ()  {
         if ( $("body").hasClass("lock")) {
@@ -18,15 +10,6 @@ $(document).ready(function () {
     });
 });
 
-/*$(document).ready(function () {     work
-    $(".menu_mobile").mousedown(function ()  {
-        if ( $("body").hasClass("lock")) {
-            $("body").removeClass("lock");
-        } else {
-            $("body").addClass("lock");
-        }
-    });
-});*/
 
 
 
